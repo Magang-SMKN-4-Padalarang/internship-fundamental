9aab5779bcf3dae2b7f1f74b2e02a84c28f2a586
Andravito Brilian Satrya <andravitobs09@gmail.com>
2026-09-07 16:33:30 +0700

    change web profile desingn and change name in index.html

--- Andravito/script.js
+++ Andravito/script.js
@@ -1,41 +1,7 @@
-const lightbox = GLightbox();
+	function bukaLightbox() {
+		document.getElementById('lightbox').style.display = 'flex';
+	}
 
-function halo() {
-    const jam = new Date().getHours();
-    const sapaan = document.getElementById("pesan");
-
-    if (jam >= 3 && jam < 11) {
-        sapaan.innerHTML = "Selamat Pagi!";
-    } else if (jam >= 11 && jam < 15) {
-        sapaan.innerHTML = "Selamat Siang!";
-    } else if (jam >= 15 && jam < 18) {
-        sapaan.innerHTML = "Selamat Sore!";
-    } else {
-        sapaan.innerHTML = "Selamat Malam!";
-    }
-
-}
-
-function popup() {
-    alert("ini adalah pesan alert");
-}
-
-let lightMode = localStorage.getItem('lightMode')
-const themeSwitch = document.getElementById('theme-switch')
-
-const enableLightmode = () => {
-    document.body.classList.add('lightMode')
-    localStorage.setItem('lightMode', 'active')
-}
-
-const disableLightmode = () => {
-    document.body.classList.remove('lightMode')
-    localStorage.setItem('lightMode', null)
-}
-
-if (lightMode === "active") enableLightmode()
-
-themeSwitch.addEventListener("click", () => {
-    lightMode = localStorage.getItem('lightMode')
-    lightMode !== "active" ? enableLightmode() : disableLightmode()
-})
+	function tutupLightbox() {
+		document.getElementById('lightbox').style.display = 'none';
+	}
