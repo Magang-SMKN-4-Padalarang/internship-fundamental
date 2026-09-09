12a451d8481a0c2de901eb1e0f2ad45dafe24392
naura.codes <naurafauziah82@gmail.com>
2026-09-09 14:24:29 +0700

    pisahkan js

---
+++ nau's project/main.js
@@ -0,0 +1,23 @@
+const btnKembali = document.getElementById("btnKembali");
+
+btnKembali.addEventListener("click", function () {
+    window.location.href = "../index.html";
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const bukaPhoto = document.getElementById('bukaPhoto');
+
+    bukaPhoto.addEventListener('click', bukaLightbox);
+
+    function bukaLightbox() {
+        document.getElementById('lightbox').style.display = 'flex';
+    }
+});
+
+function tutupLightbox() {
+    document.getElementById('lightbox').style.display = 'none';
+}
+
+
+
+
