624704386aba6f7ddef95e91befa6dab5b1ab635
zikhalSyabiLL <zikk.247@gmail.com>
2026-09-09 14:48:35 +0700

    change file

--- Zikhal/main.js
+++ Zikhal/main.js
@@ -1,3 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const lightbox = GLightbox();
+
+  const fotoProfil = document.getElementById('fotoProfil');
+  const lightbox = document.getElementById('lightbox');
+  const btnKembali = document.getElementById('btnKembali');
+
+  function bukaLightbox() {
+    lightbox.style.display = 'flex';
+  }
+
+  function tutupLightbox() {
+    lightbox.style.display = 'none';
+  }
+
+  fotoProfil.addEventListener('click', bukaLightbox);
+  lightbox.addEventListener('click', tutupLightbox);
+
+  btnKembali.addEventListener('click', function () {
+    window.location.href = '../index.html';
+  });
+
 });
