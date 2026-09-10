3464c3b083c15e36012474bf86811b1182c79d33
zikhalSyabiLL <zikk.247bdg@gmail.com>
2026-09-10 16:14:10 +0700

    change navbar

--- tentang/main.js
+++ tentang/main.js
@@ -1,9 +1,2 @@
 document.addEventListener('DOMContentLoaded', function () {
-
-  const btnKembali = document.getElementById('btnKembali');
-
-  btnKembali.addEventListener('click', function () {
-    window.location.href = '../index.html';
-  });
-
 });
